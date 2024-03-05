0feb70d60c7d17715c063d45880922030db3d103
Tom Spencer <thomas.spencer2034@gmail.com>
2024-03-05 09:46:19 -0500

    Update date insertion to events table

--- backend/src/db/schema/development.sql
+++ backend/src/db/schema/development.sql
@@ -13,8 +13,8 @@ VALUES
 
 INSERT INTO EVENTS (ID, EVENT_NAME, EVENT_DETAILS, START_TIME, EVENT_HOURS, EVENT_STATUS, EVENT_ADDRESS, CITY, EVENT_DATE, CREATOR_ID)
 VALUES 
-  (1, 'Race Setup', 'We need help setting up water stations for a marathon', '1:00PM', 4, 'INCOMPLETE', '456 status Hall Road', 'Montreal', June 1, 2024, 7),
-  (2, 'Meals on Wheels', 'We need help delivering meals to at risk clients in the community', '10:00AM', 7, 'INCOMPLETE', '5 Union Court Street', 'Vancouver', June 15, 2024,9),
-  (3, 'Ottawa Fair', 'We need people to run the info booth', '9:00AM', 8, 'INCOMPLETE', '123 Order Drive', 'Ottawa', June 11, 2024, 8),
-  (4, 'Annual Petting Zoo', 'We need people to check tickets at the door', '2:00PM', 4, 'INCOMPLETE', '476 Talus Drive', 'Vancouver', June 02, 2024, 2),
-  (5, 'Street Party Fundraiser', 'We require first aid attendants', '10:00AM', 4, 'INCOMPLETE', '1 Main Street', 'Montreal', June 22, 2024, 4);
+  (1, 'Race Setup', 'We need help setting up water stations for a marathon', '1:00PM', 4, 'INCOMPLETE', '456 status Hall Road', 'Montreal', '2024-06-01', 7),
+  (2, 'Meals on Wheels', 'We need help delivering meals to at risk clients in the community', '10:00AM', 7, 'INCOMPLETE', '5 Union Court Street', 'Vancouver', '2024-06-15',9),
+  (3, 'Ottawa Fair', 'We need people to run the info booth', '9:00AM', 8, 'INCOMPLETE', '123 Order Drive', 'Ottawa', '2024-06-11', 8),
+  (4, 'Annual Petting Zoo', 'We need people to check tickets at the door', '2:00PM', 4, 'INCOMPLETE', '476 Talus Drive', 'Vancouver', '2024-06-02', 2),
+  (5, 'Street Party Fundraiser', 'We require first aid attendants', '10:00AM', 4, 'INCOMPLETE', '1 Main Street', 'Montreal', '2024-06-22', 4);
