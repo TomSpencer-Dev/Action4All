afc31e3bb5d417527d553220c0deaf71dae19a29
Tom Spencer <thomas.spencer2034@gmail.com>
2024-03-07 11:52:40 -0500

    Add login form

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import '../styles/HomeRoute.scss';
 import Navigation from 'components/Navigation';
 import Events from 'components/Events';
+import Login from 'components/Login'
 
 const HomeRoute = (props) => {
   return (
     <div className="home-route">
       <Navigation setLoggedIn = {props.setLoggedIn} loggedIn = {props.loggedIn} />
       <Events events = {props.events} />
+      <Login/>
     </div>
   );
 };
