72b3be420886716073a78b959e9d2b129aee3556
Tom Spencer <thomas.spencer2034@gmail.com>
2024-03-07 14:11:20 -0500

    Add conditional rendering of login component

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -3,19 +3,27 @@ import React from 'react';
 import '../styles/HomeRoute.scss';
 import Navigation from 'components/Navigation';
 import Events from 'components/Events';
-import Login from 'components/Login'
+import Login from 'components/Login';
 import Register from 'components/Register';
 import EventForm from 'components/EventForm';
 
 
 const HomeRoute = (props) => {
+  
+const isLoggedIn = props.loggedIn.email
   return (
-    <div className="home-route">
-      <Navigation setLoggedIn = {props.setLoggedIn} loggedIn = {props.loggedIn} />
-      <Events events = {props.events} />
-      <Login/>
-      <Register/>
-      <EventForm/>
+    <div>
+      {isLoggedIn ? (
+        <div className="home-route">
+          <Navigation setLoggedIn={props.setLoggedIn} loggedIn={props.loggedIn} />
+          <Events events={props.events} />
+        </div>
+      ) : (
+        <div className="home-route">
+          <Navigation setLoggedIn={props.setLoggedIn} loggedIn={props.loggedIn} />
+          <Login />
+        </div>
+      )}
     </div>
   );
 };
