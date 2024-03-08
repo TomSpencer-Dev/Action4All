7f1274ea0bdaf61391f4a11f7a6418693c00661b
Tom Spencer <thomas.spencer2034@gmail.com>
2024-03-08 12:30:59 -0500

    Add sign in to google Calendar

---
+++ frontend/public/googleCalendar.js
@@ -0,0 +1,56 @@
+/* exported gapiLoaded */
+/* exported gisLoaded */
+/* exported handleAuthClick */
+/* exported handleSignoutClick */
+
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID = '362880502200-71ftj95n0j3shljlaqlsoml7qf0c2agd.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyDz5ATjo3BIjzXk9CfUcLNJI-6JKptf6K4';
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
+
+// document.getElementById('authorize_button').style.visibility = 'hidden';
+// document.getElementById('signout_button').style.visibility = 'hidden';
+
+/**
+ * Callback after api.js is loaded.
+ */
+function gapiLoaded() {
+  gapi.load('client', initializeGapiClient);
+}
+
+/**
+ * Callback after the API client is loaded. Loads the
+ * discovery doc to initialize the API.
+ */
+async function initializeGapiClient() {
+  await gapi.client.init({
+    apiKey: API_KEY,
+    discoveryDocs: [DISCOVERY_DOC],
+  });
+  gapiInited = true;
+  // maybeEnableButtons();
+}
+
+/**
+ * Callback after Google Identity Services are loaded.
+ */
+function gisLoaded() {
+  tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    callback: '', // defined later
+  });
+  gisInited = true;
+
+}
+
--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -1,12 +1,85 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Calendar() {
-  return (
-<div>
-<p>Google Calendar API Quickstart</p>
 
-</div>
- );
+const [isLoggedIn, setIsLoggedIn]=useState(false); 
+
+  /**
+   *  Sign in the user upon button click.
+   */
+  function handleAuthClick() {
+    tokenClient.callback = async (resp) => {
+      if (resp.error !== undefined) {
+        throw (resp);
+      }
+         setIsLoggedIn(true)
+      await listUpcomingEvents();
+    };
+
+    if (gapi.client.getToken() === null) {
+      // Prompt the user to select a Google Account and ask for consent to share their data
+      // when establishing a new session.
+      tokenClient.requestAccessToken({ prompt: 'consent' });
+    } else {
+      // Skip display of account chooser and consent dialog for an existing session.
+      tokenClient.requestAccessToken({ prompt: '' });
+    }
+  }
+
+  async function listUpcomingEvents() {
+    let response;
+    try {
+      const request = {
+        'calendarId': 'primary',
+        'timeMin': (new Date()).toISOString(),
+        'showDeleted': false,
+        'singleEvents': true,
+        'maxResults': 10,
+        'orderBy': 'startTime',
+      };
+      response = await gapi.client.calendar.events.list(request);
+    } catch (err) {
+      // document.getElementById('content').innerText = err.message;
+      console.log(err);
+      return;
+    }
+
+    const events = response.result.items;
+    if (!events || events.length == 0) {
+      // document.getElementById('content').innerText = 'No events found.';
+      return;
+    }
+    // Flatten to string to display
+    const output = events.reduce(
+      (str, event) => `${str}${event.summary} (${event.start.dateTime || event.start.date})\n`,
+      'Events:\n');
+    console.log(output);
+    // document.getElementById('content').innerText = output;
+  }
+
+  function handleSignoutClick() {
+    const token = gapi.client.getToken();
+    if (token !== null) {
+      google.accounts.oauth2.revoke(token.access_token);
+      gapi.client.setToken('');
+      setIsLoggedIn(false);
+      // document.getElementById('content').innerText = '';
+      // document.getElementById('authorize_button').innerText = 'Authorize';
+      // document.getElementById('signout_button').style.visibility = 'hidden';
+      console.log("User Signed Out");
+    }
+  }
+
+  return (
+    <div>
+      {
+        !isLoggedIn
+        ? <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handleAuthClick}>Sign In</button>
+        : <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handleSignoutClick}>Sign out</button>
+      }
+
+    </div>
+  );
 }
 
 export default Calendar;
