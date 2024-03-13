cae6c56880d738d1b77371abe7f3aadf409f83bd
Yordanos gebreselasie <yordibege@gmail.com>
2024-03-13 16:14:36 -0600

    Update login endpoint

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -3,9 +3,7 @@ const router = require("express").Router();
 module.exports = db => {
  
   router.get("/users", (request, response) => {
-      //const user = request.session.user; // Access the user from the session cookie
-      //const user = request.session && request.session.user;
-    //if(user){
+      
     db.query(`
       SELECT 
         id,
@@ -29,31 +27,33 @@ module.exports = db => {
 
   })
 
-  // router.get("/users/:id", (request, response) => {
-  //   const userId = request.params.id;
+  router.get("/users/:id", (request, response) => {
+    const userId = request.params.id;
 
-  //   db.query(`
-  //     SELECT 
-  //       id,
-  //       firstname,
-  //       lastname,
-  //       email,
-  //       city,
-  //       volunteer_hours
-  //     FROM users
-  //     WHERE id = $1;
-  //   `, [userId]).then(({ rows: user }) => {
-  //     if (user.length === 0) {
-  //       response.status(404).json({ error: "User not found" });
-  //     } else {
-  //       response.json(user[0]);
-  //     }
-  //   })
-  //   .catch((error) => {
-  //     console.error("Error fetching user:", error);
-  //     response.status(500).json({ error: "Internal Server Error" });
-  //   });
-  // });
+    db.query(`
+      SELECT 
+        id,
+        firstname,
+        lastname,
+        email,
+        city,
+        volunteer_hours
+      FROM users
+      WHERE id = $1;
+    `, [userId]).then(({ rows: user }) => {
+      if (user.length === 0) {
+        response.status(404).json({ error: "User not found" });
+      } else {
+        response.json(user[0]);
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching user:", error);
+      response.status(500).json({ error: "Internal Server Error" });
+    });
+  });
+
+  
   router.get("/users-by-email", (request, response) => {
     const userEmail = request.query.email;
   
