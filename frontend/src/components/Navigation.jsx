7373e26970d563371a0b06e0f97ae41439aa29f2
Tom Spencer <thomas.spencer2034@gmail.com>
2024-03-19 08:17:31 -0400

    Update nav bg color to sky blue and other styling.

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -19,7 +19,7 @@ function Navigation(props) {
   };
 
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+    <nav className="flex items-center justify-between flex-wrap bg-sky-400 p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <Link to="/">
           <img className="png" src={icon} alt="icon" title="icon" />
@@ -28,14 +28,14 @@ function Navigation(props) {
       </div>
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div className="text-sm lg:flex-grow">
-          <Link to="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+          <Link to="/" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-100 mr-4">
             Home
           </Link>
           <>
-            <Link to="/volunteer" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            <Link to="/volunteer" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-100 mr-4">
               Volunteer
             </Link>
-            <Link to="/create" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+            <Link to="/create" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-100">
               Create Event
             </Link>
           </>
@@ -43,19 +43,20 @@ function Navigation(props) {
         <div>
           {isLoggedIn ? (
             <div>
-              {userEmail}
+              <span className="text-white mr-3">{userEmail}</span>
               <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                className="bg-sky-400 text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-sky-500 hover:bg-white mt-4 lg:mt-0"
                 type="button"
                 onClick={handleLogout}
               >
                 Logout
               </button>
+
             </div>
           ) : (
             <Link
               to="/register"
-              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-sky-500 hover:bg-white mt-4 lg:mt-0"
             >
               Register
             </Link>
