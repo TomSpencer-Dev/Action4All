5211b60584ab0751e9334baaa8503a996ccbf7af
Tom Spencer <thomas.spencer2034@gmail.com>
2024-03-25 14:50:09 -0400

    Change password input type to password

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -83,7 +83,7 @@ function Register() {
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
                 Password
               </label>
-              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-event_length" type="text" placeholder="Enter password..." value={formData.password}
+              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-event_length" type="password" placeholder="Enter password..." value={formData.password}
                 onChange={handleChange} name="password" />
             </div>
           </div>
