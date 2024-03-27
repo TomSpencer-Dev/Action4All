379813deea44f923abb1d470b61232d7205e3e71
Tom Spencer <thomas.spencer2034@gmail.com>
2024-03-27 18:29:07 -0400

    Add background video

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -4,6 +4,7 @@ import Events from 'components/Events';
 import Login from 'components/Login';
 import Cookies from 'js-cookie';
 import { useLocation } from 'react-router-dom';
+import backgroundVideo from '../assets/backgroundVideo.mp4';
 
 const HomeRoute = (props) => {
   const isLoggedIn = Cookies.get('isLoggedIn');
@@ -21,13 +22,19 @@ const HomeRoute = (props) => {
             deleteEvent={props.deleteEvent} />
         </div>
         :
-        <div className="home-route">
+        <div className="home-route" style={{position: 'relative'}}>
+
           <Login setLoggedIn={props.setLoggedIn} loggedIn={props.loggedIn} />
+          <div className="video-container" style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }}>
+            <video autoPlay loop muted className='video' style={{ width: '100%', height: '100%', objectFit: 'cover' }}>
+              <source src={backgroundVideo} type='video/mp4'/>
+            </video>
+          </div>
         </div>
 
       }
     </>
-  )
+  );
 };
 
 export default HomeRoute;
